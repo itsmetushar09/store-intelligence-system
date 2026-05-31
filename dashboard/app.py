7f552277efb4ac1a0cf6f72a12ce090b2509712d
itsmetushar09 <itsmetushar307@gmail.com>
2026-05-31 23:00:11 +0530

    Add deployment config

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,7 +2,11 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000"
+import os
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000"
+)
 
 
 # --------------------------------------------------
